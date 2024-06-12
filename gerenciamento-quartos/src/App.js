136ea99e80ac7b81e2eccb54f26992d769e3aed6
JorgeBeeck <jorge.gabriel.beeck@gmail.com>
2024-06-12 17:12:35 -0300

    feat: paginas de navegacao so funcionam se estiverem logadas

--- gerenciamento-quartos/src/App.js
+++ gerenciamento-quartos/src/App.js
@@ -1,14 +1,36 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// src/App.js
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { auth } from './firebase';
 import LoginPage from './pages/LoginPage';
 import RoomManagementPage from './pages/RoomManagementPage';
 
 function App() {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(user => {
+      setUser(user);
+      setLoading(false);
+    });
+
+    return unsubscribe;
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <Router>
       <Routes>
+        <Route path="/" element={user ? <Navigate to="/rooms" /> : <LoginPage />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/rooms" element={<RoomManagementPage />} />
+        <Route
+          path="/rooms"
+          element={user ? <RoomManagementPage /> : <Navigate to="/login" />}
+        />
       </Routes>
     </Router>
   );
--- gerenciamento-quartos/src/pages/LoginPage.js
+++ gerenciamento-quartos/src/pages/LoginPage.js
@@ -1,13 +1,14 @@
 // src/pages/LoginPage.js
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { signInWithEmailAndPassword } from 'firebase/auth';
-import { auth } from '../firebase';
+import { signInWithEmailAndPassword } from 'firebase/auth';  // Corrigido import do Firebase
+import { auth } from '../firebase';  // Caminho relativo para o arquivo firebase
 
 const LoginPage = () => {
   const navigate = useNavigate();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [error, setError] = useState(null);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -15,7 +16,7 @@ const LoginPage = () => {
       await signInWithEmailAndPassword(auth, email, password);
       navigate('/rooms');
     } catch (error) {
-      console.error("Erro ao fazer login:", error);
+      setError(error.message);
     }
   };
 
@@ -28,15 +29,18 @@ const LoginPage = () => {
           placeholder="Email" 
           value={email} 
           onChange={(e) => setEmail(e.target.value)} 
+          required
         />
         <input 
           type="password" 
           placeholder="Password" 
           value={password} 
           onChange={(e) => setPassword(e.target.value)} 
+          required
         />
         <button type="submit">Login</button>
       </form>
+      {error && <p>{error}</p>}
     </div>
   );
 };
